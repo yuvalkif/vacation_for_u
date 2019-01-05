fde0d1b63fa56ad999174327fd9ca606fe0cdb14
adamCo1 <38553798+adamco1@users.noreply.github.com>
2019-01-05 16:50:47 +0200

    d

--- src/main/java/dbObjects/ATransactionMessage.java
+++ src/main/java/dbObjects/ATransactionMessage.java
@@ -1,27 +1,9 @@
 package dbObjects;
 
 public class ATransactionMessage extends AMessage {
+
     public ATransactionMessage(String sender, String reciver, String content) {
         super(sender, reciver, content);
     }
 
-    @Override
-    public String getMessageTime() {
-        return super.getMessageTime();
-    }
-
-    @Override
-    public String getSender() {
-        return super.getSender();
-    }
-
-    @Override
-    public String getReciver() {
-        return super.getReciver();
-    }
-
-    @Override
-    public String getContent() {
-        return super.getContent();
-    }
 }
